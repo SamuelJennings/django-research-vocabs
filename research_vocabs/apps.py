3986802ad96670349b0ac6e91cb4ffe9d348b9c5
Sam <samuel.scott.jennings@gmail.com>
2025-06-20 14:44:59 +0200

    fixed app name

--- research_vocabs/apps.py
+++ research_vocabs/apps.py
@@ -6,8 +6,7 @@ from django.utils.translation import gettext_lazy as _
 class ResearchVocabConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "research_vocabs"
-    verbose_name = _("LocalVocabulary")
-    verbose_name_plural = _("Vocabularies")
+    verbose_name = _("Django Vocabularies")
 
     def ready(self):
         from .utils import cache
