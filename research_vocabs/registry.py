83751f527dd3e4e4fd97a818d9d7f3703b10a53a
Sam <samuel.scott.jennings@gmail.com>
2025-06-20 14:44:39 +0200

    simplified registry

--- research_vocabs/registry.py
+++ research_vocabs/registry.py
@@ -1,19 +1,31 @@
-class VocabRegistry(dict):
-    """A registry of vocabularies that have been used in the application."""
+# class VocabRegistry(dict):
+# """A registry of vocabularies that have been used in the application."""
 
-    def __init__(self):
-        self.registry = {}
+# def __init__(self):
+#     self.registry = {}
 
-    def items(self):
-        return self.registry.items()
+# def items(self):
+#     return self.registry.items()
 
-    def register(self, vocab):
-        name = vocab.scheme().name
-        if name not in self.registry:
-            self.registry[name] = vocab
+# def register(self, vocab):
+#     name = vocab.scheme().name
+#     if name not in self.registry:
+#         self.registry[name] = vocab
 
-    def get(self, name):
-        return self.registry[name]
+# def get(self, name):
+#     return self.registry[name]
 
 
-vocab_registry = VocabRegistry()
+vocab_registry = {}
+
+
+def register(vocab):
+    """
+    Register a vocabulary in the global vocab_registry.
+
+    Args:
+        vocab (VocabularyBuilder): The vocabulary to register.
+    """
+    name = vocab.scheme().name
+    if name not in vocab_registry:
+        vocab_registry[name] = vocab
